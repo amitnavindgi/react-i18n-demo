09d144c86fec5507ba34b055a12e124d81f5c947
Amit Navindgi <amitnavindgi3@gmail.com>
2017-03-03 20:52:11 -0800

    imported and set up react-intl

--- src/server.jsx
+++ src/server.jsx
@@ -2,9 +2,37 @@ import express from 'express';
 import React from 'react';
 import ReactDom from 'react-dom/server';
 import App from './components/App';
+import cookieParser from 'cookie-parser';
+import acceptLanguage from 'accept-language';
+import { IntlProvider } from 'react-intl';
+
+//setting up English and Russian locales as supported
+acceptLanguage.languages(['en', 'ru']);
+
+//set up express to listen on port 3001
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+//log to confirm the start
+app.listen(PORT, () => {
+  console.log(`Server listening on: ${PORT}`); // eslint-disable-line no-console
+});
+
+app.use(cookieParser());
 
 const assetUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8050' : '/';
 
+//fetches a locale value from a cookie; 
+//if none is found, then the HTTP Accept-Language header is processed
+function detectLocale(req) {
+  console.log("Locale in cookie : [" + req.cookies.locale + "]");
+  const cookieLocale = req.cookies.locale;
+  console.log("Locale in req header : [" + req.headers['accept-language'] + "]");
+  return acceptLanguage.get(cookieLocale || req.headers['accept-language']) || 'en';
+}
+
+//html that is returned to the client
 function renderHTML(componentHTML) {
   return `
     <!DOCTYPE html>
@@ -22,16 +50,21 @@ function renderHTML(componentHTML) {
   `;
 }
 
-const app = express();
-
+//called when there is a request(req) made to the server
 app.use((req, res) => {
-  const componentHTML = ReactDom.renderToString(<App />);
+  const locale = detectLocale(req);
+  console.log("Locale chosen : [" + locale + "]");
+  
+  //IntlProvider child components will 
+  //have access to internationalization functions
+  const componentHTML = ReactDom.renderToString(
+    <IntlProvider locale={locale}>
+      <App />
+    </IntlProvider>
+  );
 
+  console.log("Setting cookie expiry to 10 seconds ");
+  res.cookie('locale', locale, { maxAge: 10 });
+  
   return res.end(renderHTML(componentHTML));
-});
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server listening on: ${PORT}`); // eslint-disable-line no-console
 });
